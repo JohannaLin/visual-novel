11478af24b66b44b399e6932da3aad88c852d84f
Johanna Lin <johannalinreyes@gmail.com>
2022-05-22 17:24:03 -0300

    created a function that populates HTML elements

--- pages/adventure/adventure.js
+++ pages/adventure/adventure.js
@@ -10,8 +10,7 @@ let screen = [
       `Te diré unos consejos, tómalos o déjalos, eso es tu propia decisión pero "el que avisa no triciona" y quien sabe... Quizás puedan llegar a serte muy útiles.`,
       `En primer lugar, ten cuidado con lo que ves y escuchas, no todo lo que sucede es lo que parece.
        En segundo lugar, escoge bien con quien te relacionas, en este sitio hay muchas personas que es mejor no tener de enemigo...
-       Y en tercer lugar, no importa qué suceda, no hables con la mercancía. NUNCA.`
-      `Bueno muchacho, es hora de que entres y presentarte ante el jefe. No vayas a decirle nada sobre lo que hablamos anteriormente, ¿entendido?`
+       Y en tercer lugar, no importa qué suceda, no hables con la mercancía. NUNCA.``Bueno muchacho, es hora de que entres y presentarte ante el jefe. No vayas a decirle nada sobre lo que hablamos anteriormente, ¿entendido?`,
     ],
     background: "/images/0141af056fb734940c2180d292c2fbc2.jpg",
     foreground: "/images/Gato-bicolor.png",
@@ -20,13 +19,13 @@ let screen = [
     id: "scene-1",
     name: "Innkeeper",
     dialogue: [
-        `Vaya vaya, así que este es el nuevo trabajador. ¡Bienvenido, muchacho! Espero que disfrutes de este espacio durante tu estadía trabajando.`,
-        `Hahaha, ¿sorprendido? ¿Acaso esperabas otra imagen de mí? Eso es lo que dicen todos al conocerme, los rumores me pintan de una forma muy diferente, ¿verdad? Por suerte eso a mí no me molesta.
+      `Vaya vaya, así que este es el nuevo trabajador. ¡Bienvenido, muchacho! Espero que disfrutes de este espacio durante tu estadía trabajando.`,
+      `Hahaha, ¿sorprendido? ¿Acaso esperabas otra imagen de mí? Eso es lo que dicen todos al conocerme, los rumores me pintan de una forma muy diferente, ¿verdad? Por suerte eso a mí no me molesta.
          Sería terrible si me molestase lo que el resto dice de mí, ¿no te parece, muchacho?`,
-        `Me comentaron que llegaste aquí por una oferta que pusimos en el Gremio, supongo que serás aventurero. La realidad es que no solemos contratar aventureros pero se trata de una rara excepción...`,
-        `Esta semana tenemos un evento importante y necesitamos de gente que... sea hábil. Tenemos muchas influencias de la que hacerse cargo y mucha mercancía valiosa que cuidar. Y espero que nos puedas ayudar.`,
-        `Nuevamente te doy la bienvenida a "Hotel Del Luna" y espero no tengamos ningún problema con vos también, ¿está bien, muchacho?`,
-        `¡Que disfrutes de tu estadía en este maravilloso y mágico hotel!`
+      `Me comentaron que llegaste aquí por una oferta que pusimos en el Gremio, supongo que serás aventurero. La realidad es que no solemos contratar aventureros pero se trata de una rara excepción...`,
+      `Esta semana tenemos un evento importante y necesitamos de gente que... sea hábil. Tenemos muchas influencias de la que hacerse cargo y mucha mercancía valiosa que cuidar. Y espero que nos puedas ayudar.`,
+      `Nuevamente te doy la bienvenida a "Hotel Del Luna" y espero no tengamos ningún problema con vos también, ¿está bien, muchacho?`,
+      `¡Que disfrutes de tu estadía en este maravilloso y mágico hotel!`,
     ],
     background: "/images/89699065dfcc78b45a4e2b79817a7219.png",
     foreground: "/images/Gati-blanca.png",
@@ -39,17 +38,16 @@ let screen = [
       `(Camino hacia mi habitación absorto en mis pensamientos, ordeno mi equipaje después de un largo viaje hasta el hotel y cuando dispongo a relajarme en la cama escucho un sonido extraño en la habitación.)`,
       `(Me acerco al lugar de donde provenía el ruido y me encuentro con una sorpresa...)`,
     ],
-    background: "/images/RenderInteriorConcept15-1920x1080-be448c2077a2576eb46d68d4d4100323.jpg",
+    background:
+      "/images/RenderInteriorConcept15-1920x1080-be448c2077a2576eb46d68d4d4100323.jpg",
     foreground: null,
   },
   {
     id: "scene-3",
     name: "Mysterious Character",
-    dialogue: [
-        `zzz...`,
-        `...`,
-    ],
-    background: "/images/RenderInteriorConcept15-1920x1080-be448c2077a2576eb46d68d4d4100323.jpg",
+    dialogue: [`zzz...`, `...`],
+    background:
+      "/images/RenderInteriorConcept15-1920x1080-be448c2077a2576eb46d68d4d4100323.jpg",
     foreground: "/images/Gato-colorado-atigrado.png",
   },
 ];
